db53d4a843e4a7e42185d31af12af66eea2cb07f
enanesh <edna.ganesha.gr@gmail.com>
2023-02-11 18:51:33 -0800

    Model 'User' created, I still need to validate that the REGEX used to validate password actually works

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: (email) => {
+                    const pattern = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+                    return pattern.test(email);
+                },
+                message: (props) => `${props.value} is not in correct format !!`,
+            },
+        },
+        toughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+        
+    }
+);
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;

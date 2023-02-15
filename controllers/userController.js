8f7e3cac0baf5ea36912b08a7831eb641e6403f9
enanesh <edna.ganesha.gr@gmail.com>
2023-02-14 22:07:00 -0800

    Adds getUserById to the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,37 @@ const { User, Thought } = require("../models");
 
 module.exports = {
     
-    //Get All users 
-    getAllUsers (req, res) {
+    //Gets All users 
+    getAllUsers(req, res) {
         User.find({})
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
         
 
         
+    },
+
+    // Gets a single user 
+    getUserById(req, res) {
+        User.findOne({ _id: req.params.userId })
+            // populate contains douments from another collection
+            .populate("thoughts")
+            .populate("friends")
+            // selects any populated paths for you 
+            .select('-__v')
+            //removes empty objects
+            .lean()
+            .then(async (user) =>
+                !  user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json( user )
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+
+
+
     }
 }    

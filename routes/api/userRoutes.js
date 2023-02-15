f10c8f144b9d0d90291e820874be26532f57c1f2
enanesh <edna.ganesha.gr@gmail.com>
2023-02-15 00:40:14 -0800

    Sets the  Route to get user by Id, tested and  working properly

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,13 +2,18 @@ const router = require('express').Router();
 
 const {
     getAllUsers,
-   
+    getUserById,
     createUser
 
-} = require('../../controllers/userController')
+} = require('../../controllers/userController');
 
 
-router.route('/').get(getAllUsers).post(createUser)
+
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:userId').get(getUserById)
+
 
 
 

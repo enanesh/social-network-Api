564cacb7ce544cd5a4edf4c72a09a72ddda8209f
enanesh <edna.ganesha.gr@gmail.com>
2023-02-15 17:00:29 -0800

    Creates the route /api/thought/:toughtId updateThought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,6 +5,7 @@ const {
     getThoughts,
     createThought,
     getThoughtsById,
+    updateThought,
 
 
 } = require('../../controllers/thoughtController');
@@ -17,6 +18,6 @@ router.route('/').get(getThoughts).post(createThought);
 
 // /api/thought/:thoughtId
 
-router.route('/:thoughtId').get(getThoughtsById)
+router.route('/:thoughtId').get(getThoughtsById).put(updateThought);
 
 module.exports = router;
